e939d8fb2e69514c33878cccc36a60db9719bd9c
fraser <ch.p.fraser@gmail.com>
2017-01-13 16:17:19 +0100

    some other stuff

---
+++ animalKingdomManiaFightClub/src/com/company/Mamals/Lion.java
@@ -0,0 +1,12 @@
+package com.company.Mamals;
+
+/**
+ * Created by fraser on 1/13/17.
+ */
+public class Lion extends Mammal{
+
+    public Lion(){
+        population = 40;
+        procreationPower= 1;
+    }
+}
---
+++ animalKingdomManiaFightClub/src/com/company/Mamals/Mammal.java
@@ -0,0 +1,10 @@
+package com.company.Mamals;
+
+
+import com.company.Animal;
+
+public abstract class Mammal extends Animal {
+    public Mammal(){
+    }
+
+}
